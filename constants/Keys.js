0e0d379ce401deedfaf98c1bb75b1abed1389767
charles-ramos <charles@back4app.com>
2021-03-30 11:56:13 -0300

    update keys.js

--- constants/Keys.js
+++ constants/Keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    applicationId: 'lOnJ1j6c5OHzz7RFmlXATbEqGvKA3BuT9KXGyWdO',
-    javascriptKey: 'n1u4b9uVkjUKq9QlZcKWfiHrf45zY1PsegXOaoT7',
-    serverURL: 'https://parseapi.back4app.com/'
+    applicationId: 'Parse App ID here',
+    javascriptKey: 'Javascript Key here',
+    serverURL: 'Server URL here'
    }
